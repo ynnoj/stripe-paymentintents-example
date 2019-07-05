ee344ff089594732129d9a6a0ce99015948652ee
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-05 10:41:35 +0100

    fix: Refactor processing state so that Stripe Element is never hidden
    Otherwise handleCardPayment() won't work

--- app/components/CheckoutForm.js
+++ app/components/CheckoutForm.js
@@ -7,18 +7,28 @@ import tw from 'tailwind.macro'
 
 const Button = styled.button`
   ${tw`bg-blue-500 border-0 cursor-pointer ml-4 px-4 py-2 rounded text-base text-white`}
+
+  ${({ disabled }) => (disabled ? tw`bg-blue-200` : tw`bg-blue-500`)}
+`
+const Alert = styled.div`
+  ${tw`border border-solid mt-4 p-2 rounded text-sm`}
 `
 const Section = styled.section`
   ${tw`bg-white mx-auto px-4 py-4 rounded shadow w-2/3 md:w-1/3`}
 `
 const StyledCardElement = styled(CardElement)`
   ${tw`border border-gray-300 border-solid flex-auto p-2 rounded`}
+
+  ${({ disabled }) => (disabled ? tw`opacity-50` : tw`opacity-100`)}
 `
 const StyledForm = styled.form`
   ${tw`flex items-center`}
 `
-const Error = styled.div`
-  ${tw`border border-red-200 border-solid bg-red-100 mt-4 p-2 rounded text-red-400 text-sm`}
+const Error = styled(Alert)`
+  ${tw`border-red-200 bg-red-100 text-red-400`}
+`
+const Processing = styled(Alert)`
+  ${tw`border-blue-200 bg-blue-100 text-blue-400`}
 `
 
 function CheckoutForm({ stripe }) {
@@ -57,15 +67,19 @@ function CheckoutForm({ stripe }) {
         render={({ handleSubmit, submitting }) => {
           return (
             <React.Fragment>
-              {checkoutProcessing ? (
-                'Loading'
-              ) : (
-                <StyledForm onSubmit={handleSubmit}>
-                  <StyledCardElement onReady={el => setCardElement(el)} />
-                  <Button type="submit">Submit</Button>
-                </StyledForm>
-              )}
+              <StyledForm onSubmit={handleSubmit}>
+                <StyledCardElement
+                  onReady={el => setCardElement(el)}
+                  disabled={submitting}
+                />
+                <Button type="submit" disabled={submitting}>
+                  Submit
+                </Button>
+              </StyledForm>
 
+              {checkoutProcessing && (
+                <Processing>Processing your submission</Processing>
+              )}
               {checkoutError && <Error>{checkoutError}</Error>}
             </React.Fragment>
           )
